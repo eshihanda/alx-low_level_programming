e05c5977fd59adfd9751290ef4d4abca70376b6c
eshihanda <eshihandamonn@gmail.com>
2023-03-19 09:14:38 -0700

    print all possible different combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entrty point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 1; y < 10; y++)
+		{
+			if (x < y && x != y)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+				if (x == 8 && y == 9)
+					continue;
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

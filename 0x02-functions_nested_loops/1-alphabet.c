e497d7921ebbf5235f4196873fe5975a6aafaa39
eshihanda <eshihandamonn@gmail.com>
2023-03-21 15:02:30 -0700

    the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return: void
+ *
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+}

a33318086238fa775f63374f0286845a4be95e48
eshihanda <eshihandamonn@gmail.com>
2023-03-22 13:13:55 -0700

    addition function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two values
+ * @n1: the first number to be added
+ * @n2: the second number to be added
+ * Return: sum of the values
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}

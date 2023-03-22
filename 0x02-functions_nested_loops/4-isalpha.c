6d7d997c344285c6f1e74d3ca5b1eb0272b53ab8
eshihanda <eshihandamonn@gmail.com>
2023-03-21 17:44:32 -0700

    all alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - function to print alphabet character
+ *
+ * @c: the character to be checked
+ *
+ * Return: 1 for alphabets or 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c >= 122))
+		return (1);
+	else
+		return (0);
+}

3655629acadb4c45102423e626b1eafebe8acece
eshihanda <eshihandamonn@gmail.com>
2023-03-22 15:09:58 -0700

    print all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,16 +20,18 @@ void print_to_98(int n)
 			else if (x == 98)
 				printf("%d\n", x);
 		}
+		printf("\n");
 	}
 	else if (n >= 98)
 	{
-		for (x = n; x >= 98; x++)
+		for (x = n; x >= 98; x--)
 		{
 			if (x != 98)
 				printf("%d, ", x);
 			else if (x == 98)
 				printf("%d\n", x);
 		}
+		printf("\n");
 	}
 }
 

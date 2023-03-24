e1786e8405d94ada76762c23ddb7f87e9f573488
eshihanda <eshihandamonn@gmail.com>
2023-03-23 18:52:34 -0700

    only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that print 0 to 14
+ *
+ * Return: always 0
+ */
+void more_numbers(void)
+{
+	int x;
+	int z;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (z = 0; z <= 14; z++)
+		{
+			if (z >= 10)
+			{
+				_putchar(z / 10 + '0');
+			}
+			_putchar(z % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}

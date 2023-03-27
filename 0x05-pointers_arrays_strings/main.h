6ee6d6be9f42d2658b12256004e2c9ca4599d17e
eshihanda <eshihandamonn@gmail.com>
2023-03-27 12:10:07 -0700

    battery st

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
+int main(void);
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -13,3 +12,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+#endif

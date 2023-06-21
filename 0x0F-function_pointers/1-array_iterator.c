fdd48e6fe0a8a4ec5ee837163389cc05d09ca750
eshihanda <eshihandamonn@gmail.com>
2023-06-21 13:34:20 -0700

    function to execute a function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterate each elements of an array of functions
+ * @size: the size of the array
+ * @action: pointer to the function
+ * @array: array to iterate
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int j;
+
+	for (j = 0; j < size; j++)
+	{
+		action(array[j]);
+	}
+}

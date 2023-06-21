4de8069f68744280dfa78623818835c3cd11c888
eshihanda <eshihandamonn@gmail.com>
2023-06-21 13:39:03 -0700

    function to execute a function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int j;
 
+	if (array == NULL || action == NULL)
+		return;
 	for (j = 0; j < size; j++)
 	{
 		action(array[j]);

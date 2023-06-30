47bad8ae420c8b662626e916d21f47b78f715657
eshihanda <eshihandamonn@gmail.com>
2023-06-30 15:02:35 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,12 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h->str == NULL)
-	{
-		printf("[0](nil)\n");
-	}
 	while (h != NULL)
 	{
-		printf("[%u] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0](nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}

0dec885504687b9c5639449cf763d340a3da58e0
eshihanda <eshihandamonn@gmail.com>
2023-10-02 09:37:27 -0700

    function that reads file and prints to STDOUT

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - function that reads a text file
+ * @filename: the file to be read
+ * @letters: the number of letters to read and print
+ *
+ * Return: number of letters, otherwise 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	ssize_t fd;
+	ssize_t r, w;
+
+	if (filename == NULL)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	r = read(fd, buff, letters);
+	w = write(STDOUT_FILENO, buff, r);
+
+	if (fd == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buff);
+		return (0);
+	}
+	free(buff);
+	close(fd);
+	return (w);
+}
+
